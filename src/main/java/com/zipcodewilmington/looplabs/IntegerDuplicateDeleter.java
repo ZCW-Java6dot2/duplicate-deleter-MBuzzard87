4cb3e9177eab0a67d1e00724d337be032866a605
buzz <buzz@zipcoders-MacBook-Pro.local>
2020-12-02 21:06:23 -0500

    passed

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,59 @@
 package com.zipcodewilmington.looplabs;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    private Integer[] removeArr;
+
+    public IntegerDuplicateDeleter(Integer[] intArr) {
+        super(intArr);
+        this.removeArr = intArr;
+    }
+
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        List<Integer> intList = new ArrayList<>();
+        List<Integer> storeRemovedInt = new ArrayList<>();
+        for (Integer addToList : removeArr) {
+            intList.add(addToList);
+        }
+        for (Integer freqInt : intList) {
+            if (Collections.frequency(intList,freqInt) >= maxNumberOfDuplications) {
+                storeRemovedInt.add(freqInt);
+            }
+        }
+        intList.removeAll(storeRemovedInt);
+        Integer[] updatedArr = new Integer[intList.size()];
+        intList.toArray(updatedArr);
+        return updatedArr;
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        List<Integer> intList = new ArrayList<>();
+        List<Integer> storeRemovedInt = new ArrayList<>();
+        for (Integer addToList : removeArr) {
+            intList.add(addToList);
+        }
+        for (Integer freqInt : intList) {
+            if (Collections.frequency(intList,freqInt) == exactNumberOfDuplications) {
+                storeRemovedInt.add(freqInt);
+            }
+        }
+        intList.removeAll(storeRemovedInt);
+        Integer[] updatedArr = new Integer[intList.size()];
+        intList.toArray(updatedArr);
+        return updatedArr;
+    }
+
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,8 +1,57 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    private String[] strArray;
+
+    public StringDuplicateDeleter(String[] strArray) {
+        super(strArray);
+        this.strArray = strArray;
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        List<String> strList = new ArrayList<>();
+        List<String> storeRemovedStr = new ArrayList<>();
+        for (String addToList : strArray) {
+            strList.add(addToList);
+        }
+        for (String freqStr : strList) {
+            if (Collections.frequency(strList,freqStr) >= maxNumberOfDuplications) {
+                storeRemovedStr.add(freqStr);
+            }
+        }
+        strList.removeAll(storeRemovedStr);
+        String[] updatedArr = new String[strList.size()];
+        strList.toArray(updatedArr);
+        return updatedArr;
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        List<String> strList = new ArrayList<>();
+        List<String> storeRemovedStr = new ArrayList<>();
+        for (String addToList : strArray) {
+            strList.add(addToList);
+        }
+        for (String freqStr : strList) {
+            if (Collections.frequency(strList,freqStr) == exactNumberOfDuplications) {
+                storeRemovedStr.add(freqStr);
+            }
+        }
+        strList.removeAll(storeRemovedStr);
+        String[] updatedArr = new String[strList.size()];
+        strList.toArray(updatedArr);
+        return updatedArr;
+    }
+
+
+
 }
